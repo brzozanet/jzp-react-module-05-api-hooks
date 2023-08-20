f25468af5b00f96d8c2c21ec07a470d51787f1e5
filipMaM <filip.mamcarczyk@gmail.com>
2023-08-20 12:14:22 +0200

    Lesson 3

--- fe/src/components/Panel/Panel.jsx
+++ fe/src/components/Panel/Panel.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
-import { List } from "../List/List";
 import styles from "./Panel.module.css";
+import { List } from "../List/List";
 import { Form } from "../Form/Form";
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 
 export function Panel() {
     const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         fetch("http://localhost:3000/words")
             .then((res) => res.json())
@@ -27,11 +30,31 @@ export function Panel() {
             });
     }
 
+    function handleDeleteItem(id) {
+        fetch(`http://localhost:3000/words/${id}`, { method: "DELETE" })
+            .then((res) => {
+                if (res.ok) {
+                    setData((prevData) =>
+                        prevData.filter((item) => item.id !== id)
+                    );
+                } else {
+                    throw new Error("Błąd podczas usuwania! Spróbuj ponownie.");
+                }
+            })
+            .catch((error) => {
+                setError(error.message);
+                setTimeout(() => {
+                    setError(null);
+                }, 3000);
+            });
+    }
+
     return (
         <>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
             <section className={styles.section}>
                 <Form onFormSubmit={handleFormSubmit} />
-                <List data={data}></List>
+                <List data={data} onDeleteItem={handleDeleteItem} />
             </section>
         </>
     );
