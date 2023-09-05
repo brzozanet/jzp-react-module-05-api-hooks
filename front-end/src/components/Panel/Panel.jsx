5efdd91717d719ddedd27449635db1c92c95abd9
filipMaM <filip.mamcarczyk@gmail.com>
2023-09-05 18:43:10 +0200

    Lesson 5

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -14,13 +14,20 @@ export function Panel() {
     const [selectedCategory, setSelectedCategory] = useState(null);
 
     useEffect(() => {
+        let isCanceled = false;
         const params = selectedCategory ? `?category=${selectedCategory}` : "";
         fetch(`${url}${params}`)
             .then((res) => res.json())
             .then((res) => {
-                setData(res);
-                setIsLoading(false);
-            });
+                if (!isCanceled) {
+                    setData(res);
+                    setIsLoading(false);
+                }
+            }); 
+
+        return () => {
+            isCanceled = true;
+        };
     }, [selectedCategory]);
 
     function handleFormSubmit(formData) {
