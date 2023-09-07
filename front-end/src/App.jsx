3379393ceb0e5cdb5a87d082b41b4bdefc8385fd
filipMaM <filip.mamcarczyk@gmail.com>
2023-09-07 20:38:39 +0200

    Lesson 9

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import { Panel } from "./components/Panel/Panel";
 import { Button } from "./components/Button/Button";
 import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage";
+import { Timer } from "./components/Timer/Timer";
 
 function App() {
     const [isPanelShown, setIsPanelShown] = useState(true);
@@ -26,6 +27,7 @@ function App() {
                 {isPanelShown ? "Schowaj panel" : "Pokaż panel"}
             </Button>
             {isPanelShown && <Panel onError={handleError} />}
+            <Timer />
         </main>
     );
 }
